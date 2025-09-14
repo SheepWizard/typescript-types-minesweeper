1ef7a063926e10eceb23b1f7d395da5268c2660f
sheep_wizard <you@example.com>
2025-09-14 17:02:55 +0100

    readme

--- run.sh
+++
@@ -1,4 +0,0 @@
-docker stop ts-minesweeper
-docker rm ts-minesweeper
-docker build -t ts-minesweeper --network=host .
-docker run -d --network nginxproxymanager_default --name=ts-minesweeper --restart unless-stopped -d ts-minesweeper
